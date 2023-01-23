0492f89b08c0d1898836bba7c8230e267440835c
colino17 <51090962+colino17@users.noreply.github.com>
2023-01-23 14:43:34 -0400

    Update xteve.sh

--- xteve.sh
+++ xteve.sh
@@ -3,10 +3,10 @@
 XTEVE_FILE=/config/xteve.conf
 
 if [ -f "$XTEVE_FILE" ]; then
-	xteve -port=34400 -config=$XTEVE_FILE
+	xteve -port=34400 -config=/config
 else
 	cp /sample.conf /config/xteve.conf
-	xteve -port=34400 -config=$XTEVE_FILE
+	xteve -port=34400 -config=/config
 fi
 
 exit
